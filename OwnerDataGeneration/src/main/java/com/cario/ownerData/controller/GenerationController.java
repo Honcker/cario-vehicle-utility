c38339ac4e0d845eda70db01ea5fe9bf5102fcf6
rkukean-cario <145584511+rkukean-cario@users.noreply.github.com>
2023-10-27 15:21:44 -0400

    SpringImplemented

---
+++ OwnerDataGeneration/src/main/java/com/cario/ownerData/controller/GenerationController.java
@@ -0,0 +1,9 @@
+package com.cario.ownerData.controller;
+import org.springframework.beans.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenerationController {
+
+}
--- OwnerDataGeneration/src/main/java/org/example/OwnerDataGeneration.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class OwnerDataGeneration {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
