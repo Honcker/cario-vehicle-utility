4bbfc2c7ff940b42c5e0e572fe7f6f226911eb32
rkukean-cario <145584511+rkukean-cario@users.noreply.github.com>
2023-10-30 13:44:37 -0400

    controller made

--- OwnerDataGeneration/src/main/java/com/cario/ownerData/controller/GenerationController.java
+++ OwnerDataGeneration/src/main/java/com/cario/ownerData/controller/GenerationController.java
@@ -1,16 +1,25 @@
 package com.cario.ownerData.controller;
-import org.springframework.beans.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.cario.ownerData.generation.GenerationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class GenerationController {
-    private final List<Vehicle> vehicleList;
+    @PostMapping("/upload-excel")
+    public ResponseEntity<String> uploadExcel(@RequestParam("file") MultipartFile file) {
+        if (file != null && !file.isEmpty()) {
+            try {
 
-    @RequestMapping("get xlsx")
-    public
-
-
+                return new ResponseEntity<>("Excel file uploaded successfully!", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Failed to process the Excel file: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>("Please upload a valid Excel file.", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- OwnerDataGeneration/src/main/java/com/cario/ownerData/generation/GenerationService.java
+++ OwnerDataGeneration/src/main/java/com/cario/ownerData/generation/GenerationService.java
@@ -5,4 +5,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class GenerationService {
 
+    
+
+    private class Vehicle {
+        private String ownerMail;
+        private String VIN;
+        private String title;
+        private String issueDate;
+        private int weight;
+        private int number;
+        private String body;
+        private String make;
+        private String model;
+        private int year;
+        private int mileage;
+        private String email;
+        private boolean lien;
+    }
+
 }
