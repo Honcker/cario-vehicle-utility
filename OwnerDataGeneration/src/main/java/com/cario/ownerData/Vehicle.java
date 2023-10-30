55905369b4719fdeb5aa67b1ad135edb2a14e302
rkukean-cario <145584511+rkukean-cario@users.noreply.github.com>
2023-10-30 10:40:29 -0400

    eod

---
+++ OwnerDataGeneration/src/main/java/com/cario/ownerData/Vehicle.java
@@ -0,0 +1,10 @@
+package com.cario.ownerData;
+
+public class Vehicle {
+    private String ownerMail;
+    private String VIN;
+    private String title;
+    private String issueDate;
+    private int weight;
+    private int number;
+}
--- OwnerDataGeneration/src/main/java/com/cario/ownerData/controller/GenerationController.java
+++ OwnerDataGeneration/src/main/java/com/cario/ownerData/controller/GenerationController.java
@@ -1,9 +1,16 @@
 package com.cario.ownerData.controller;
 import org.springframework.beans.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GenerationController {
+    private final List<Vehicle> vehicleList;
+
+    @RequestMapping("get xlsx")
+    public
+
 
 }
